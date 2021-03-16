336457bf7e8792b5af1674597ae67ab421abaf3e
MrCrashh <80788354+MrCrashh@users.noreply.github.com>
2021-03-16 23:18:28 +0200

    post-punk

---
+++ post-punk.js
@@ -0,0 +1,75 @@
+function show ( name )
+{
+  var elem = document.getElementById(name); 
+  if ( elem ) 
+    elem.style.display ="block";
+}
+
+
+function hide ( name )
+{
+  var elem = document.getElementById(name); 
+  if ( elem ) 
+    elem.style.display = "none";
+}
+
+function show1 ( name )
+{
+  var elem = document.getElementById(name); 
+  if ( elem ) 
+    elem.style.display ="block";
+}
+
+
+function hide1 ( name )
+{
+  var elem = document.getElementById(name); 
+  if ( elem ) 
+    elem.style.display = "none";
+}
+
+function show2 ( name )
+{
+  var elem = document.getElementById(name); 
+  if ( elem ) 
+    elem.style.display ="block";
+}
+
+
+function hide2 ( name )
+{
+  var elem = document.getElementById(name); 
+  if ( elem ) 
+    elem.style.display = "none";
+    
+}
+
+function show3 ( name )
+{
+  var elem = document.getElementById(name); 
+  if ( elem ) 
+    elem.style.display ="block";
+}
+
+
+function hide3 ( name )
+{
+  var elem = document.getElementById(name); 
+  if ( elem ) 
+    elem.style.display = "none";
+}
+
+function show4 ( name )
+{
+  var elem = document.getElementById(name); 
+  if ( elem ) 
+    elem.style.display ="block";
+}
+
+
+function hide4 ( name )
+{
+  var elem = document.getElementById(name); 
+  if ( elem ) 
+    elem.style.display = "none";
+}
